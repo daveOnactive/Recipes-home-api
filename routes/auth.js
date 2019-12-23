bba77988d5d06941e92a547c734d9f84df4c1da2
daveOnactive <chinweike89@gmail.com>
2019-12-23 22:49:02 +0100

    validations

--- routes/auth.js
+++ routes/auth.js
@@ -1,29 +1,29 @@
 const router = require('express').Router();
 const User = require('../model/User');
-
-//VALIDATION
-const Joi = require('@hapi/joi');
-
-const schema = {
-  name: Joi.string().min(6).required(),
-  email: Joi.string().min(6).required().email(),
-  password: Joi.string().min(6).required()
-};
+const { registerValidation } = require('../validation');
 
 router.post('/register', async (req, res) => {
-  const validation = Joi.validate(req.body, schema);
-  res.send(validation);
-  // const user = new User({
-  //   name: req.body.name,
-  //   email: req.body.email,
-  //   password: req.body.password
-  // });
-  // try {
-  //   const saveUser = await user.save();
-  //   res.send(saveUser)
-  // } catch(err) {
-  //   res.status(400).send(err);
-  // }
+  // Validate User Details
+  // const { error } = registerValidation(req.body);
+
+  // Check if email already exist
+  const emailExist = await User.findOne({ email: req.body.email });
+  if(emailExist) {
+    return res.status(400).send('Email already exist in the DB');
+  }
+
+  // Create User
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
+  try {
+    const saveUser = await user.save();
+    res.send(saveUser)
+  } catch(err) {
+    res.status(400).send(err);
+  }
 
 });
 
