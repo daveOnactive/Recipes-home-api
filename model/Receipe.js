549f9066471c2af412e6ecf67df9c10d902b8cae
daveOnactive <chinweike89@gmail.com>
2019-12-25 22:05:03 +0100

    receipe schema added

--- model/Receipe.js
+++ model/Receipe.js
@@ -7,4 +7,32 @@ const receipeSchema = new mongoose.Schema({
     min: 6,
     max: 225
   },
-})
+  description: {
+    type: String,
+    required: true,
+    min: 6
+  },
+  ingredient: {
+    type: [String],
+    required: true
+  },
+  BeforeYouCook: {
+    type: [String],
+    required: true
+  },
+  CookingDirections: {
+    type: [String],
+    required: true
+  },
+  tools : {
+    type: [String],
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    min: 6
+  }
+});
+
+module.exports = mongoose.model('Receipe', receipeSchema);
