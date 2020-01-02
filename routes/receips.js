36800561a91494076791d82711ca00d366efed41
daveOnactive <chinweike89@gmail.com>
2020-01-02 22:18:57 +0100

    update

--- routes/receips.js
+++ routes/receips.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const receipeRoute = require('../controllers/receipeRoute');
 
-<<<<<<< HEAD
+
 router.get('/recipe', receipeRoute.getAllReceipe );
 
 router.get('/recipe/:id', receipeRoute.getOneReceipe );
@@ -11,16 +11,5 @@ router.patch('/recipe/:id', receipeRoute.editReceipe );
 router.delete('/recipe/:id', receipeRoute.deleteReceipe );
 
 router.post('/recipe', receipeRoute.addReceipe );
-=======
-router.get('/receipe', receipeRoute.getAllReceipe );
-
-router.get('/receipe/:id', receipeRoute.getOneReceipe );
-
-router.put('/receipe/:id', receipeRoute.editReceipe );
-
-router.delete('/recipe/:id', receipeRoute.deleteReceipe );
-
-router.post('/receipe', receipeRoute.addReceipe );
->>>>>>> 0d2a9044a9a6085a20e93ec657999f1b76bec6d0
 
 module.exports = router;
